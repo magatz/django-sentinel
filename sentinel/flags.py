b0686c1d862f6a255861e2bb09c0580172737c81
Andrea Magatti <andrea.magatti@gmail.com>
2014-12-05 10:16:11 +0100

    timezone

--- sentinel/flags.py
+++ sentinel/flags.py
@@ -37,7 +37,7 @@ def load_flags(addresses=None):
         c += 1
         key = normalised_flag_key(a.ip)
         if a.expiry:
-            timeout = total_seconds(datetime.datetime.utcnow() - a.expiry)
+            timeout = total_seconds(datetime.utcnow() - a.expiry)
             if timeout > 0:
                 cache.set(key, a.flag, timeout)
         else:
