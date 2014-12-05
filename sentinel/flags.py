46275ee527c6074aa973dfd685adc2c1b2d8e5df
Andrea Magatti <andrea.magatti@gmail.com>
2014-12-05 10:14:54 +0100

    timezone

--- sentinel/flags.py
+++ sentinel/flags.py
@@ -1,4 +1,6 @@
 import time
+from django.utils.timezone import utc
+import datetime 
 from ipaddr import *
 from django.core.cache import cache
 from models import *
@@ -35,7 +37,7 @@ def load_flags(addresses=None):
         c += 1
         key = normalised_flag_key(a.ip)
         if a.expiry:
-            timeout = total_seconds(datetime.now() - a.expiry)
+            timeout = total_seconds(datetime.datetime.utcnow() - a.expiry)
             if timeout > 0:
                 cache.set(key, a.flag, timeout)
         else:
